eef319e8130175ded99bd5257673faedd0209793
Yuxin Shen <yuxinshen_98@outlook.com>
2024-03-22 23:46:27 +0000

    init build

---
+++ docs/assets/js/mathjax_config.js
@@ -0,0 +1,1 @@
+window.MathJax={tex:{tags:"ams",inlineMath:[["$","$"],["\\(","\\)"]],macros:{md:["{\\mathop{}\\mathopen{\\mathrm{d}}}"],bR:["{\\mathbf{R}}"],bbR:["{\\mathbb{R}}"],E:["{\\mathbf{E}}"],bbP:["{\\mathbb{P}}"]}},svg:{fontCache:"global"}};
